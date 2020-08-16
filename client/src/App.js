cf31fbe10ee09a1102bf894a3dbf6852be0a6fd4
diplesh <dipleshmankape@gmail.com>
2020-08-16 11:00:46 +0530

    Planning about Components is mentioned in App

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       hello world
+      {/* Login Based upon the userType */}
+      {/* Dashboard -> CreateForm for admins   */}
+      {/* Dashboard -> Select Data Analysis Page based upon Project Name */}
+      {/* Data Analysis Component : Display data in some good Format : Graphs or Heatmaps */}
+      {/* Form filling page for Field Workers */}
     </div>
   );
 }
