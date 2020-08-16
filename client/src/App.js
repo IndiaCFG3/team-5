cf76dd06309e12586ee1961fb045ddf0b2ee8d4c
diplesh <dipleshmankape@gmail.com>
2020-08-16 16:05:48 +0530

    Created the Dashboard and Analytics Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
+import Dashboard from './Dashboard';
+
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <h1>Navigate to dashboard2.html </h1>
       {/* Login Based upon the userType */}
       {/* Dashboard -> CreateForm for admins   */}
       {/* Dashboard -> Select Data Analysis Page based upon Project Name */}
       {/* Data Analysis Component : Display data in some good Format : Graphs or Heatmaps */}
       {/* Form filling page for Field Workers */}
+
+
+
+      {/* <Dashboard /> */}
+
     </div>
   );
 }
---
+++ client/src/Landing_Page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Landing_Page() {
+    return (
+        <div>
+
+            <h1>Hello This is the Landing Page</h1>
+
+
+
+
+
+
+        </div>
+    )
+}
+
+export default Landing_Page
